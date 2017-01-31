1f80c790a420a7846ff6292488da14e13b6d9226
Mikhail <moovix@rambler.ru>
2017-01-31 14:39:23 +0100

    ds
    ds

---
+++ java/Sample.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2016 Polarion AG
+ */
+package com.siemens.polarion.ect.tests;
+
+public class Sample {
+
+	/**
+	 * Linked to {@wi.implements VV-1910:123 no project }
+	 * wseedfdsewdsss
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException();
+    }
+	
+	/**
+	 * Linked to {@wi.tests VV-1910 tests something}
+     */
+	public void render() {
+		
+	}
+	
+}
---
+++ java/Sample2.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2016 Polarion AG
+ */
+package com.siemens.polarion.ect.tests;
+
+public class Sample {
+ 
+ 	/**
+     * Linked to {@polarion.wi test/DPP-2000 - changed message}
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException();
+    }
+
+	/**
+     * Linked to {@polarion.wi nottest/DPP-2000 - changed message}
+     */
+	public String getSmth() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException();
+    }
+	
+}
